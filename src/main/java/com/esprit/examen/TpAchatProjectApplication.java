637eca1940a069296475e8f78df0abb78858fb40
Cyrine Nasri <cyrine.nasri@esprit.tn>
2022-11-03 17:37:08 +0100

    test1

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+
+
+
 @EnableScheduling
 @SpringBootApplication
 public class TpAchatProjectApplication {
