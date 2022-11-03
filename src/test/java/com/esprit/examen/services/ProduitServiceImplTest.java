13d01cd9f0496b661beeac274884f4715271b930
Chayma Boualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-11-03 17:09:20 +0100

    Update ProduitServiceImplTest.java

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -4,14 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.repositories.ProduitRepository;
-
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
