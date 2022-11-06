17b21e9fbffda78e0f869f2eab1b000f23000eb0
khaledsaidi <khaled.saidi2@esprit.tn>
2022-11-06 23:15:57 +0100

    fix test

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableScheduling
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TpAchatProjectApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.esprit.examen.entities.Facture;
-import com.esprit.examen.entities.Produit;
 import com.esprit.examen.repositories.FactureRepository;
 
 @SpringBootTest
