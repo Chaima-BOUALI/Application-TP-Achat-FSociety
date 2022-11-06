6ec9b38136ee535f3c53869fcd9d96146ebecf47
khaledsaidi <khaled.saidi2@esprit.tn>
2022-11-06 22:48:49 +0100

    ss

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -3,11 +3,12 @@ package com.esprit.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TpAchatProjectApplication {
 
 	public static void main(String[] args) {
