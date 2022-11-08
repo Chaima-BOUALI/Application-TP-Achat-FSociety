5305483c30cc8acd90e369fea1aff7dd6c634d85
khaledsaidi <khaled.saidi2@esprit.tn>
2022-11-08 04:38:29 +0100

    testin23

---
+++ src/test/java/com/esprit/examen/services/PushTest.java
@@ -0,0 +1,22 @@
+package com.esprit.examen.services;
+
+public class PushTest {
+/*
+ * *
+ * @Test
+	public void testAssignProduitToStock() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date dateCreation = dateFormat.parse("15/10/2022");
+		Stock s = new Stock("stock1",10,200);
+		Stock savedStock= stockService.addStock(s);
+		Produit p = new Produit(null, "Prod1", "voiture", 0, dateCreation, null, null, null, null);
+		Produit produit = produitService.addProduit(p);
+		log.info("Stock: "+savedStock);
+		log.info("Produit: "+produit);
+		produitService.assignProduitToStock(produit.getIdProduit(), savedStock.getIdStock());
+		//assertNotNull(produit.getStock());
+		//stockService.deleteStock(savedStock.getIdStock());
+		produitService.deleteProduit(produit.getIdProduit());
+	     log.info("Product assigned to stock successfully");
+ */
+}
